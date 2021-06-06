8b243246fb5c8cbff5bfe95a6cccacdbe5fc5f3d
Maxim Rybas <maxrybas@gmail.com>
2021-06-06 16:56:22 +0300

    Added heatmap for CIVILIZATION bard game dices

---
+++ dice_proba.py
@@ -0,0 +1,25 @@
+class Dice:
+    def __init__(self, dim, coef=1, modif=0):
+        self.dim = dim
+        self.coef = coef
+        self.modif = modif
+        self.min = coef + modif
+        self.max = coef * dim + modif
+
+    def __repr__(self):
+        return f"Dice {self.coef}d{self.dim}+{self.modif}"
+
+
+def prob_equal(dice: Dice, value):
+    if value < dice.min or value > dice.max:
+        return 0
+    return 1 / dice.dim
+
+
+def prob_less(dice: Dice, value):
+    return sum([prob_equal(dice, j) for j in range(1, value)])
+
+
+def prob_win(dice_1: Dice, dice_2: Dice):
+    return (1 / dice_1.dim) * sum([prob_less(dice_2, i) for i in range(dice_1.min, dice_1.max + 1)])
+
